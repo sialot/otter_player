3e3bb78750fb21ba9623493be6fe9aededfb1cda
sialot <sialot@sina.com>
2019-03-27 15:36:06 +0800

    调整缓存大小

--- aac_decoder.c
+++ aac_decoder.c
@@ -75,63 +75,65 @@ int aac_decode_func(void * pDecoder, FRAME_DATA * pPesPkt, PRIORITY_QUEUE *queue
 	uint8_t *data = (uint8_t *)pPesPkt->data;
 	size_t data_size = (size_t)pPesPkt->len;
 
-	// 解析数据获得一个Packet， 从输入的数据流中分离出一帧一帧的压缩编码数据
-	int ret = av_parser_parse2(d->parser, d->context, &d->pkt->data, &d->pkt->size,
-		data, data_size,
-		pPesPkt->pts, pPesPkt->dts, 0);
+	while (data_size > 0) {
 
-	if (ret < 0) {
-		printf("Error while parsing\n");
-		return -1;
-	}
-	data += ret;
-	data_size -= ret;
+		// 解析数据获得一个Packet， 从输入的数据流中分离出一帧一帧的压缩编码数据
+		int ret = av_parser_parse2(d->parser, d->context, &d->pkt->data, &d->pkt->size,
+			data, data_size,
+			pPesPkt->pts, pPesPkt->dts, 0);
 
-	if (d->pkt->size)
-	{
-		int ret;
-		ret = avcodec_send_packet(d->context, d->pkt);
 		if (ret < 0) {
-			printf("Error submitting the packet to the decoder\n");
+			printf("Error while parsing\n");
 			return -1;
 		}
-		
-		/* read all the output frames (in general there may be any number of them */
-		while (ret >= 0) {
-			ret = avcodec_receive_frame(d->context, d->decoded_frame);
-			if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
-				return 0;
-			else if (ret < 0) {
-				printf("Error during decoding\n");
-				return -1;
-			}
-			data_size = av_get_bytes_per_sample(d->context->sample_fmt);
-			if (data_size < 0) {
-				/* This should not occur, checking just for paranoia */
-				printf("Failed to calculate data size\n");
+		data += ret;
+		data_size -= ret;
+
+		if (d->pkt->size)
+		{
+			int ret;
+			ret = avcodec_send_packet(d->context, d->pkt);
+			if (ret < 0) {
+				printf("Error submitting the packet to the decoder\n");
 				return -1;
 			}
 
-			// 准备入参
-			int out_len = d->decoded_frame->nb_samples * d->context->channels * data_size;
-			unsigned char * out_data = malloc(sizeof(unsigned char) * out_len);
-
-			for (int i = 0; i < d->decoded_frame->nb_samples; i++)
-			{
-				for (int ch = 0; ch < d->context->channels; ch++)
-				{
-					memcpy(out_data + (data_size * (i * 2 + ch)), d->decoded_frame->data[ch] + data_size * i, data_size);
+			/* read all the output frames (in general there may be any number of them */
+			while (ret >= 0) {
+				ret = avcodec_receive_frame(d->context, d->decoded_frame);
+				if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
+					continue;
+				else if (ret < 0) {
+					printf("Error during decoding\n");
+					return -1;
 				}
+				data_size = av_get_bytes_per_sample(d->context->sample_fmt);
+				if (data_size < 0) {
+					/* This should not occur, checking just for paranoia */
+					printf("Failed to calculate data size\n");
+					return -1;
+				}
+
+				// 准备入参
+				int out_len = d->decoded_frame->nb_samples * d->context->channels * data_size;
+				unsigned char * out_data = malloc(sizeof(unsigned char) * out_len);
+
+				for (int i = 0; i < d->decoded_frame->nb_samples; i++)
+				{
+					for (int ch = 0; ch < d->context->channels; ch++)
+					{
+						memcpy(out_data + (data_size * (i * 2 + ch)), d->decoded_frame->data[ch] + data_size * i, data_size);
+					}
+				}
+
+				FRAME_DATA *out_frame = frame_data_create(pPesPkt->av_type, 0x01, pPesPkt->dtime, pPesPkt->ptime, out_data, out_len);
+				out_frame->channels = d->context->channels;
+				priority_queue_push(queue, out_frame, out_frame->ptime);
+
+				///printf("get pcm data.nb_samples:%d, channels:%d, data_size:%d, total_size:%d \n ", d->decoded_frame->nb_samples, d->context->channels, (int)data_size, js_frame_data_len);
 			}
-
-			FRAME_DATA *out_frame = frame_data_create(pPesPkt->av_type, 0x01, pPesPkt->dtime, pPesPkt->ptime, out_data, out_len);
-			out_frame->channels = d->context->channels;
-			priority_queue_push(queue, out_frame, out_frame->ptime);
-
-			///printf("get pcm data.nb_samples:%d, channels:%d, data_size:%d, total_size:%d \n ", d->decoded_frame->nb_samples, d->context->channels, (int)data_size, js_frame_data_len);
 		}
 	}
-
 	return 0;
 }
 

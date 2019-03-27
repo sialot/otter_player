c03dad3666c0771036c530e36ef16f119a8fe88a
sialot <37317169+sialot@users.noreply.github.com>
2019-03-27 22:17:30 +0800

    调整缓存大小

--- decoder_master.c
+++ decoder_master.c
@@ -1,10 +1,10 @@
 #include "decoder_master.h"
 
 // 最大缓存帧数，音频+视频
-const int MAX_BUFFER_FRAME_COUNT = 100;
+const int MAX_BUFFER_FRAME_COUNT = 132;
 
 // 最小预加载帧数
-const int MAX_PREBUFFER_FRAME_COUNT = 20;
+const int MAX_PREBUFFER_FRAME_COUNT = 24;
 
 // 创建对象
 DECODER_MASTER * decoder_master_create(int display_width, int display_height)
